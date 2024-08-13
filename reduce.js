fe761120a8308943aa5bef6ef9be73fd470fef12
RatifiedGeorge <kamungegeorge42@gmail.com>
2024-08-13 16:07:40 +0300

    Added more on the reduce function

--- reduce.js
+++ reduce.js
@@ -9,5 +9,26 @@ const prices = [5, 30, 10, 25, 15, 20];
 //     return accumulator + element;
 // }
 
-const total = prices.reduce((accumulator, element) => accumulator + element);
-console.log(total); // 105
+// const total = prices.reduce((accumulator, element) => accumulator + element);
+// console.log(total); 
+const totalPrice = prices.reduce((total, price) => {
+    return total + price;
+}, 0)
+console.log(totalPrice); 
+
+const people = [
+    { name: "John", age: 30 },
+    { name: "Jane", age: 25 },
+    { name: "Jim", age: 20 },
+    { name: "June", age: 30 },
+    { name: "Jill", age: 25 }
+];
+
+const grouped = people.reduce((groupedPeople, person) => {
+    const age = person.age;
+    if (groupedPeople[age] == null) groupedPeople[age] = [];
+    groupedPeople[age].push(person);
+    return groupedPeople;
+}, {});
+
+console.log(grouped);
